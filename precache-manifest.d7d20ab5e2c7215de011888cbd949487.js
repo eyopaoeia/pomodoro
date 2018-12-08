910df96256b58bc0d6e5bee03bf58e95c2efeaa0
Tassja Blore <njblore@outlook.com>
2018-12-08 16:50:37 +0000

    Updates

---
+++ precache-manifest.d7d20ab5e2c7215de011888cbd949487.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffd90bec43c88f1cfa0b",
+    "url": "/pomodoro/static/css/main.2ef43366.chunk.css"
+  },
+  {
+    "revision": "ffd90bec43c88f1cfa0b",
+    "url": "/pomodoro/static/js/main.ffd90bec.chunk.js"
+  },
+  {
+    "revision": "77958300264c6869a197",
+    "url": "/pomodoro/static/js/1.77958300.chunk.js"
+  },
+  {
+    "revision": "a702e67ff471c6ecfc33",
+    "url": "/pomodoro/static/js/runtime~main.a702e67f.js"
+  },
+  {
+    "revision": "f021c6bef3c505a7cba8d54101ba699a",
+    "url": "/pomodoro/static/media/wet-tomato.f021c6be.jpg"
+  },
+  {
+    "revision": "5090e12bd4d9a7907495b35b8bde013c",
+    "url": "/pomodoro/index.html"
+  }
+];
